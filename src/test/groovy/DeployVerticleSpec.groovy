12e3fd1db672c62579138871045a4eb757123064
Ming Cheng <mingcheng@users.noreply.github.com>
2019-09-18 10:16:13 +0800

    升级到 Java11 以及更新类库版本

--- src/test/groovy/DeployVerticleSpec.groovy
+++ src/test/groovy/DeployVerticleSpec.groovy
@@ -1,64 +1,63 @@
 import com.google.common.base.Charsets
 import com.google.common.io.Resources
 import io.vertx.core.DeploymentOptions
-import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
+import io.vertx.reactivex.core.Vertx
 import spock.lang.Specification
 import spock.util.concurrent.AsyncConditions
 
 class DeployVerticleSpec extends Specification {
-    Vertx vertx
-    AsyncConditions conditions
+  Vertx vertx
+  AsyncConditions conditions
 
-    def setup() {
-        vertx = Vertx.vertx()
-    }
+  def setup() {
+    vertx = Vertx.vertx()
+  }
 
-    def "Deply ServerVerticle"() {
-        setup:
-        conditions = new AsyncConditions(2);
+  def "Deply ServerVerticle"() {
+    setup:
+    conditions = new AsyncConditions(2)
 
+    expect:
+    DeploymentOptions options = new DeploymentOptions()
+    URL url = Resources.getResource("application.json")
+    options.setConfig(new JsonObject(Resources.toString(url, Charsets.UTF_8)))
 
-        expect:
-        DeploymentOptions options = new DeploymentOptions();
-        URL url = Resources.getResource("application.json");
-        options.setConfig(new JsonObject(Resources.toString(url, Charsets.UTF_8)));
+    assert options.getConfig().containsKey("debug")
+    assert options.getConfig().containsKey("services")
 
-        assert options.getConfig().containsKey("debug")
-        assert options.getConfig().containsKey("services")
+    vertx.deployVerticle("com.gracecode.scaffold.verticle.ServerVerticle", options) { response ->
+      conditions.evaluate {
+        assert response.succeeded()
+      }
 
-        vertx.deployVerticle("com.gracecode.scaffold.verticle.ServerVerticle", options) { response ->
-            conditions.evaluate {
-                assert response.succeeded()
-            }
-
-            vertx.setTimer(5l, {
-                vertx.undeploy(response.result()) { undeployResponse ->
-                    conditions.evaluate {
-                        assert undeployResponse.succeeded()
-                    }
-                }
-            })
+      vertx.setTimer(5l, {
+        vertx.undeploy(response.result()) { undeployResponse ->
+          conditions.evaluate {
+            assert undeployResponse.succeeded()
+          }
         }
+      })
+    }
 
-        vertx.deployVerticle("com.gracecode.scaffold.verticle.ConsumerVerticle", options) { response ->
-            conditions.evaluate {
-                assert response.succeeded()
-            }
+    vertx.deployVerticle("com.gracecode.scaffold.verticle.ConsumerVerticle", options) { response ->
+      conditions.evaluate {
+        assert response.succeeded()
+      }
 
-            vertx.setTimer(5l, {
-                vertx.undeploy(response.result()) { undeployResponse ->
-                    conditions.evaluate {
-                        assert undeployResponse.succeeded()
-                    }
-                }
-            })
+      vertx.setTimer(5l, {
+        vertx.undeploy(response.result()) { undeployResponse ->
+          conditions.evaluate {
+            assert undeployResponse.succeeded()
+          }
         }
-
-        conditions.await(10d)
+      })
     }
 
-    def cleanup() {
-        vertx.close()
-    }
+    conditions.await(10d)
+  }
+
+  def cleanup() {
+    vertx.close()
+  }
 }
